af1ed681bb39442cd9ed88972b19176807d909cc
owen <owen.blanchandin@gmail.com>
2026-06-10 20:16:01 +0100

    added start to roulette

---
+++ static/roulette.js
@@ -0,0 +1,15 @@
+var bet = 0;
+var betOn = null;
+
+const landedOnText = document.getElementById("landedOnText");
+const rollBtn = document.getElementById("rollBtn");
+
+function rollTheWheel() {
+    let landOn = Math.floor(Math.random() * 38);
+    if (landOn === 37) {
+        landOn = "00";
+    }
+    landedOnText.textContent = `Landed on: ${landOn}`;
+}
+
+rollBtn.addEventListener("click", rollTheWheel);
